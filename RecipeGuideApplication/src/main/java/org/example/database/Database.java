2da07a5dcdc1b7966f8593c85626904ae307562a
yarenSuludag <153204710+yarenSuludag@users.noreply.github.com>
2025-02-19 23:46:28 +0300

    Update Database.java

--- RecipeGuideApplication/src/main/java/org/example/database/Database.java
+++ RecipeGuideApplication/src/main/java/org/example/database/Database.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/tarifrehberi?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false&allowPublicKeyRetrieval=true"; // Veritabanı URL
     private static final String USER = "root";
-    private static final String PASSWORD = "fb123456";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
